126c05392a0b18543c5b6c6714d2e20e7d7605b7
Baran Önen <baranonenn@gmail.com>
2020-07-20 21:48:44 +0300

    Initial Commit

---
+++ Barculator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Barculator
+//
+//  Created by Baran Önen on 20.07.2020.
+//  Copyright © 2020 Baran Önen. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
